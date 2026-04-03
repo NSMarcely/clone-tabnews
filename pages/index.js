d7913778d5afe402d901f84e4bbbc7ea719f7e26
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-04-03 10:40:04 -0300

    :sparkles: feat: adicionar arquivo .prettierignore

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <h1>Thanks for the course 🩷</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
 export default Home;
